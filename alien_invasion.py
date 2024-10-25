36ded3b0191f0802b2d8f6b8a735600fdd9e8dc8
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-26 00:59:44 +0100

    can move the ship right, continuously

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,6 +21,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
 
@@ -29,6 +30,9 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
 
 
     def _update_screen(self):
