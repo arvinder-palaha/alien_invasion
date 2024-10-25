e1c0426263041c95430c4ff7c544cec86fcbf137
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-26 00:47:19 +0100

    fixed order of setting settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,12 +9,11 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.settings = Settings()
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         
-        # set the background color.
-        self.settings = Settings()
 
     def run_game(self):
         """Start the main loop for the game."""
