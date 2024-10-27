34f2b8ef7f1a23da0ea678eecf8ac91acc01848e
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-27 19:59:25 +0000

    adjusted bullet speed to be bit quicker, slightly fewer rows of aliens per fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -57,7 +57,7 @@ class AlienInvasion:
         
         # Determine the number of rows of aliens that fit on the screen.
         ship_height = self.ship.rect.height
-        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        available_space_y = (self.settings.screen_height - (4 * alien_height) - ship_height)
         number_rows = available_space_y // (2 * alien_height)
         
         # Create the full fleet of aliens.
