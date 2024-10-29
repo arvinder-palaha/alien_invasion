218183161ad49f7fca0ebfb7adaa29b83fa6dfaa
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-10-29 00:01:03 +0000

    increase score at increase seetings. track all ships in scoring if multiple hit.

--- alien_invasion.py
+++ alien_invasion.py
@@ -168,6 +168,7 @@ class AlienInvasion:
             
             self.stats.game_active = True
             self.stats.reset_stats()
+            self.sb.prep_score()
             
             # get rid of any remaining aliens and bullets
             self.aliens.empty()
@@ -200,7 +201,8 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
 
         if not self.aliens:
